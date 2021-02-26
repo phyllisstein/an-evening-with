2e6ea65efbcc65f9c45828109b9c87354f463759
Daniel P. Shannon <daniel@fleet.i.ng>
2021-02-26 17:49:24 -0500

    🚨 Two spaces, delint.

--- src/hooks/hammer/use-hammer.js
+++ src/hooks/hammer/use-hammer.js
@@ -2,17 +2,17 @@ import { useEffect, useRef } from 'react'
 import { Manager } from 'hammerjs'
 
 export const useHammer = target => {
-    const mc = useRef(
-        new Manager(target ?? document.body, {
-            recognizers: [],
-        }),
-    )
+  const mc = useRef(
+    new Manager(target ?? document.body, {
+      recognizers: [],
+    }),
+  )
 
-    useEffect(() => {
-        mc.current.set({ enable: true })
+  useEffect(() => {
+    mc.current.set({ enable: true })
 
-        return () => mc.current.destroy()
-    }, [''])
+    return () => mc.current.destroy()
+  }, [''])
 
-    return mc.current
+  return mc.current
 }
